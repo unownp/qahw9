d177eb88596f7df88bebc689fd6951ab727ff33c
Mark Mark <antiutka@gmail.com>
2022-01-24 01:55:38 +0300

    2

--- src/test/java/np/qa/lesson9/tests/TestBase.java
+++ src/test/java/np/qa/lesson9/tests/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
         Configuration.startMaximized = true;
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl="https://demoqa.com";
-   //     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
