275c6cd50ddb4b73caef925aeda95159f8973f87
Mark Mark <antiutka@gmail.com>
2022-01-24 02:00:40 +0300

    дополнение к TestBase

--- src/test/java/np/qa/lesson9/tests/TestBase.java
+++ src/test/java/np/qa/lesson9/tests/TestBase.java
@@ -1,6 +1,8 @@
 package np.qa.lesson9.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import np.qa.lesson9.helpers.Attach;
 import np.qa.lesson9.pages.components.FixedBanComponent;
 import org.junit.jupiter.api.AfterEach;
@@ -10,11 +12,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.startMaximized = true;
-        Configuration.holdBrowserOpen = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        //Configuration.startMaximized = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.baseUrl="https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+        Configuration.browserSize = "1920x1080";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
