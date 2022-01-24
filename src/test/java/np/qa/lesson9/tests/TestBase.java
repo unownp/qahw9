1d95fa0d85a3b7553bac7cb310c1e4c5112cf6af
Mark Mark <antiutka@gmail.com>
2022-01-24 13:31:14 +0300

    дополнение к TestBase

--- src/test/java/np/qa/lesson9/tests/TestBase.java
+++ src/test/java/np/qa/lesson9/tests/TestBase.java
@@ -1,6 +1,7 @@
 package np.qa.lesson9.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import np.qa.lesson9.helpers.Attach;
@@ -14,7 +15,7 @@ public class TestBase {
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         //Configuration.startMaximized = true;
-        Configuration.holdBrowserOpen = false;
+        //Configuration.holdBrowserOpen = true;
         Configuration.baseUrl="https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.browserSize = "1920x1080";
@@ -30,6 +31,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        Selenide.closeWebDriver();
     }
     FixedBanComponent fixedBanComponent=new FixedBanComponent();
 
